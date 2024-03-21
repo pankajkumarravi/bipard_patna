de32c822208c60ebd52a15f7291827e5b2475e0a
Pankaj Kumar Ravi <38873221+pankajkumarravi@users.noreply.github.com>
2024-03-21 13:06:34 +0530

    ds code added

---
+++ pyhtonClass.py
@@ -0,0 +1,12 @@
+
+
+class Teacher:
+    pass
+teacher=Teacher
+teacher.name="Pankaj Kumar Ravi"
+teacher.school="UHS Mirzapur"
+
+print(teacher.__name__)
+print(teacher.school)
+#print(teacher.__class__)
+#print(teacher.__call__)
